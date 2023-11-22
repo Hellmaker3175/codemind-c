01e40eac68197c2a60f69d68bf596edeebf4fa21
Hellmaker3175 <147417847+Hellmaker3175@users.noreply.github.com>
2023-11-22 11:00:32 +0530

    #include<stdio.h> int main() {     int a,i;
        scanf("%d",&a);
        for (i=1; i<=a;i++)
        {
            if (i%2==0)
            {
                printf("");
            }
            else
            {
                printf("%d ",i);
            }
        }
    }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    
+    scanf("%d",&a);
+    for (i=1; i<=a;i++)
+    {
+        if (i%2==0)
+        {
+            printf("");
+        }
+        else
+        {
+            printf("%d ",i);
+        }
+    }
+}
